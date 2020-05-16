a86c1625a539dd9a5c8f7416d7e801d433e5b079
Anatole Lamarque <lamarqueanatole@gmail.com>
2020-05-16 11:52:40 +0200

    [CONFIG] new varenvs

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,5 +8,7 @@ if (res.error) {
 
 export default {
     port: process.env.PORT || 8080,
-    databaseURL: process.env.DATABASE_URL || 'mongodb://localhost/test'
+    databaseURL: process.env.DATABASE_URL || 'mongodb://localhost/test',
+    saltRounds: process.env.SALT_ROUNDS || 10,
+    secret: process.env.SECRET || 'kaiserschmarrn'
 };
